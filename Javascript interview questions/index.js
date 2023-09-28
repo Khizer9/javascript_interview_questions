c9fac54408aa83ff17c9a4798a179973eee8c8ca
Khizer Ali <khizer358@gmail.com>
2023-09-28 10:12:25 +0500

    added some more

--- Javascript interview questions/index.js
+++ Javascript interview questions/index.js
@@ -233,3 +233,20 @@
 // }
 // let averageNum = sum / arr.length
 // console.log(averageNum)
+
+
+// Reverse a string without keyword
+let myName = 'my name is khizer ali'
+
+// for(i = myName.length - 1; i>=0; i--){
+//     console.log(myName[i])
+// }
+
+// Second Method
+
+// let i = myName.length - 1
+
+// while(i>=0){
+//     console.log(myName[i])
+//     i--
+// }
